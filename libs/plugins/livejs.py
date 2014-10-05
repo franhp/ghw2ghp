6ad50005d6391ef7de6a62a5fcf0f9f4e0cc043c
Fran Hermoso <franhp@gmail.com>
2014-10-05 20:42:51 +0100

    Disable livejs by default

--- libs/plugins/livejs.py
+++ libs/plugins/livejs.py
@@ -3,7 +3,7 @@ from libs.plugins import WikiPlugins
 
 class WikiPlugin(WikiPlugins):
     tag_name = 'livejs'
-    active = True
+    active = False
 
     def website_tag(self, website_context):
         return '<script type="text/javascript" src="http://livejs.com/live.js"></script>'
